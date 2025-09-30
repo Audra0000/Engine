3105c5c1b8d72f994813de1af1169ee4f15af749
Ana Alcaraz <alcarazmartinezana@gmail.com>
2025-09-30 20:27:46 +0200

    Application prep

--- Engine/Application.h
+++ Engine/Application.h
@@ -1,5 +1,39 @@
 #pragma once
-class Application
+
+#include <vector>
+
+enum update_status
 {
+	UPDATE_CONTINUE = 1,
+	UPDATE_STOP,
+	UPDATE_ERROR
 };
 
+class Module;
+class ModuleWindow;
+
+class Application
+{
+public:
+
+	ModuleWindow* window;
+	
+
+private:
+
+	std::vector<Module*> list_modules;
+	
+
+public:
+
+	Application();
+	~Application();
+
+	bool Init();
+	update_status Update();
+	bool CleanUp();
+
+private:
+
+	void AddModule(Module* module);
+};
